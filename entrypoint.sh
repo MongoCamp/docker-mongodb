9e8cadcd1280325d62d9653df08958bf5aedafc7
QuadStingray <github@quadstingray.com>
2018-06-28 22:33:40 +0200

    fix in command

--- entrypoint.sh
+++ entrypoint.sh
@@ -45,7 +45,7 @@ if [[ -z ${1} ]]; then
   mongod --port ${MONGO_PORT}  --fork --syslog --dbpath ${MONGO_DATA_DIR} 2>&1
 
   echo "Set Version to 4.0"
-  mongo admin --port ${MONGO_PORT}  --eval "db.adminCommand( { setFeatureCompatibilityVersion: "4.0" } ); db.adminCommand( { getParameter: 1, featureCompatibilityVersion: 1 } );"
+  mongo admin --port ${MONGO_PORT}  --eval 'db.adminCommand( { setFeatureCompatibilityVersion: "4.0" } ); db.adminCommand( { getParameter: 1, featureCompatibilityVersion: 1 } );'
 
   if [[ ${MONGO_ROOT_PWD} != 'NONE' && ${MONGO_ROOT_PWD} != '' ]]; then
 
