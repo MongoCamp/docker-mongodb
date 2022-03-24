2537e43742f96c839c19d851e9de6b8d6bb0e341
QuadStingray <github@quadstingray.com>
2022-03-24 14:11:08 +0100

    fix: Startup set Feature Version to 5.0

--- entrypoint.sh
+++ entrypoint.sh
@@ -81,8 +81,9 @@ if [[ -z ${1} ]]; then
   echo "[entrypoint.sh] Starting mongod for upgrade Informations"
   mongod --port ${MONGO_PORT} --fork --syslog --dbpath ${MONGO_DATA_DIR} 2>&1
 
-  echo "[entrypoint.sh] Set Version to 4.2"
-  mongo admin --port ${MONGO_PORT}  --eval 'db.adminCommand( { setFeatureCompatibilityVersion: "4.2" } ); db.adminCommand( { getParameter: 1, featureCompatibilityVersion: 1 } );'
+  echo "[entrypoint.sh] Set Version to 5.0"
+  mongo admin --port ${MONGO_PORT}  --eval 'db.adminCommand( { set
+  CompatibilityVersion: "5.0" } ); db.adminCommand( { getParameter: 1, featureCompatibilityVersion: 1 } );'
 
   if [[ ${MONGO_ROOT_PWD} != 'NONE' && ${MONGO_ROOT_PWD} != '' ]]; then
     echo "[entrypoint.sh] Admin User to Database"
